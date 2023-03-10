41fa1fa4b403a6da530a7f2ae61661a2492a9eed
Simon Locht Nørregaard <noerregaard.simon@gmail.com>
2023-03-10 10:13:02 +0100

    Add project files.

---
+++ AsymetricCryptologyReciever/Program.cs
@@ -0,0 +1,18 @@
+﻿using AssymetricCyptologyReciever;
+using System;
+using System.Text;
+
+var rsa = new RsaWithXmlKey();
+
+const string publicKeyPath = "c:\\temp\\publickey.xml";
+const string privateKeyPath = "c:\\temp\\privatekey.xml";
+rsa.AssignKey(publicKeyPath, privateKeyPath);
+
+Console.WriteLine(File.ReadAllText(publicKeyPath));
+Console.ReadLine();
+
+Console.WriteLine("Indtast den krypterede besked.");
+string encryptedMessage = Console.ReadLine();
+byte[] decryptedBytes = rsa.Decrypt(privateKeyPath, encryptedMessage);
+Console.WriteLine("Decrypted besked:");
+Console.WriteLine(Encoding.UTF8.GetString(decryptedBytes));
---
+++ AsymetricCryptologyReciever/RsaWithXmlKey.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssymetricCyptologyReciever
+{
+	public class RsaWithXmlKey
+	{
+		public void AssignKey(string publicKeyPath, string privateKeyPath)
+		{
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+
+				var publicKeyfolder = Path.GetDirectoryName(publicKeyPath);
+				var privateKeyfolder = Path.GetDirectoryName(privateKeyPath);
+
+				if (!Directory.Exists(publicKeyfolder))
+				{
+					Directory.CreateDirectory(publicKeyfolder);
+				}
+
+				if (!Directory.Exists(privateKeyfolder))
+				{
+					Directory.CreateDirectory(privateKeyfolder);
+				}
+
+				if (!File.Exists(privateKeyPath))
+				{
+					File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+				}
+
+				if (!File.Exists(publicKeyPath))
+				{
+					File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+
+				}
+			}
+		}
+
+		public byte[] Decrypt(string privateKeyPath, string dataToDecrypt)
+		{
+			byte[] dataAsBytesArray = new byte[dataToDecrypt.Length];
+			dataAsBytesArray = Convert.FromBase64String(dataToDecrypt);
+
+			byte[] plain;
+
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+				rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+				plain = rsa.Decrypt(dataAsBytesArray, false);
+				return plain;
+			}
+
+		}
+	}
+}
